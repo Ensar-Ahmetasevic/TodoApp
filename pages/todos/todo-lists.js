49b8c7fac48d7a43ba70bc7a868729ec11d8cf58
Ensar Ahmetasevic <111744786+Ensar-Ahmetasevic@users.noreply.github.com>
2023-05-22 13:58:53 +0200

    lofing spinner for todo lists

--- pages/todos/todo-lists.js
+++ pages/todos/todo-lists.js
@@ -4,8 +4,14 @@ import { getServerSideProps } from "@/helpers/getServerSideProps";
 import Layout from "@/components/layout/layout";
 import TodoAllLists from "@/components/todo/todo-lists/todo-all-lists";
 import TodoListForm from "@/components/todo/todo-lists/todo-list-form";
+import LoadingSpinner from "@/helpers/loading-spiner";
+import TodoListQuery from "@/requests/requests-for-todo-lists/todo-list-query";
 
 function TodoListsHP() {
+  const { isLoading, isError, error } = TodoListQuery();
+
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return <ErrorNotification error={error} />;
   return (
     <Layout>
       <Fragment>
