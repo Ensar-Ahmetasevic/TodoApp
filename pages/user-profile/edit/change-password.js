84f7765fdb5e308cde4f43155a0887e51f9d4c6a
Ensar Ahmetasevic <111744786+Ensar-Ahmetasevic@users.noreply.github.com>
2023-05-24 17:23:22 +0200

    Redy for deploy

---
+++ pages/user-profile/edit/change-password.js
@@ -0,0 +1,5 @@
+function ChangerPassword() {
+  return <h1>In progress</h1>;
+}
+
+export default ChangerPassword;
---
+++ pages/user-profile/edit/delete-user.js
@@ -0,0 +1,5 @@
+function DeleteUser() {
+  return <h1>In progress</h1>;
+}
+
+export default DeleteUser;
